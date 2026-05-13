cdaa4726f6d30f6593ea4852e011b74b5fde2c7b
furuikeharuki <furuike.haruki@gmail.com>
2026-05-13 15:48:52 +0900

    feat: add instant black background loading for movie detail page

---
+++ apps/web/app/movies/[slug]/loading.tsx
@@ -0,0 +1,14 @@
+export default function MovieDetailLoading() {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: "52px",
+        left: 0,
+        right: 0,
+        bottom: 0,
+        background: "#0a0a0a",
+      }}
+    />
+  );
+}

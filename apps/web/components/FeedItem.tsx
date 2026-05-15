fe1ff341a3ad13b246d063f318d3aba89264e220
furuikeharuki <furuike.haruki@gmail.com>
2026-05-15 13:47:17 +0900

    fix: feed-videoを object-fit:contain に変更し画面内に収まるように修正

--- apps/web/components/FeedItem.tsx
+++ apps/web/components/FeedItem.tsx
@@ -473,7 +473,7 @@ const itemStyle = `
     inset: 0;
     width: 100%;
     height: 100%;
-    object-fit: cover;
+    object-fit: contain;
     object-position: center center;
     border-radius: 8px;
   }
